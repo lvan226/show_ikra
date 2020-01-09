e981f3853af6b2d2c809b61f2022aa3cef9cbdf3
lvan226 <lvan226@list.ru>
2020-01-09 20:32:45 +0800

    Added gallery

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,7 @@ gulp.task('style', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.css'
     // 'node_modules/magnific-popup/dist/magnific-popup.css'
   ])
   .pipe(concat('libs.min.css'))
@@ -33,6 +34,7 @@ gulp.task('style', function(){
 gulp.task('script', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js'
     // 'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
   ])
   .pipe(concat('libs.min.js'))
